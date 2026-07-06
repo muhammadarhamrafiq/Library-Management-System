313babbb485781c278907770994494119416e2be
muhammadarhamrafiq <arhmraf@gmail.com>
2026-07-06 17:05:02 +0500

    feat(cli): Added cli to wire up services

---
+++ app/cli/commands/__init__.py
@@ -0,0 +1,5 @@
+from .book_cli import BookCli
+from .loan_cli import LoanCli
+from .user_cli import UserCli
+
+__all__ = ["BookCli", "LoanCli", "UserCli"]
--- app/main.py
+++ app/main.py
@@ -1,15 +1,4 @@
-import asyncio
-
-from sqlalchemy import text
-
-from app.core.database import SessionLocal
-
-
-async def main():
-    async with SessionLocal() as session:
-        result = await session.execute(text("SELECT 1"))
-        print(result.scalar())
-
+from app.cli.main import main
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
